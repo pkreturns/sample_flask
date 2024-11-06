b143dc19e158d7cdb88feb7a1b87eabedd57501f
pkreturns <84262852+pkreturns@users.noreply.github.com>
2024-11-07 03:47:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World12fsakhsdb'
+    return 'Hello World12fsakhsdbffffffff'
 
 # main driver function
 if __name__ == '__main__':
